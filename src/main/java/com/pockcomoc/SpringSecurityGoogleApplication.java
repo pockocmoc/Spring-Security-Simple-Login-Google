bac4226d44cc114fdbe4ffef73b63aea36640a17
pockocmoc <pockocmoc83@gmail.com>
2024-02-12 15:56:02 +0300

    initial commit

---
+++ src/main/java/com/pockcomoc/SpringSecurityGoogleApplication.java
@@ -0,0 +1,13 @@
+package com.pockcomoc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityGoogleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurityGoogleApplication.class, args);
+	}
+
+}
